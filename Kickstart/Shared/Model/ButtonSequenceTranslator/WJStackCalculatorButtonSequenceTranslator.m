295e1ad72a87a4b4249e61e1d3e9762696f29ecd
Antoni Kedracki <akedracki@gmail.com>
2013-10-08 22:11:15 +0200

    exercise 4A - button sequence translator implemented

--- Kickstart/Shared/Model/ButtonSequenceTranslator/WJStackCalculatorButtonSequenceTranslator.m
+++ Kickstart/Shared/Model/ButtonSequenceTranslator/WJStackCalculatorButtonSequenceTranslator.m
@@ -10,8 +10,51 @@
 #import "WJStackCalculatorButtonSequenceTranslator.h"
 
 
-@implementation WJStackCalculatorButtonSequenceTranslator {
+@interface WJStackCalculatorButtonSequenceTranslator ()
 
+@property (nonatomic, assign, readwrite) CGFloat value;
+
+@end
+
+@implementation WJStackCalculatorButtonSequenceTranslator {
+    BOOL _dotPressed;
+    CGFloat _fractionModifier;
 }
+
+@synthesize value = _value;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self clear];
+    }
+
+    return self;
+}
+
+
+- (void)inputDigit:(NSUInteger)digit {
+    if(digit > 9){
+        @throw [NSException exceptionWithName:@"IllegalArgumentException" reason:@"digit out of valid range[0..9]" userInfo:nil];
+    }
+
+    if(!_dotPressed){
+        self.value = _value * 10.0f + digit;
+    } else {
+        _fractionModifier *= 0.1f;
+        self.value = _value + (float)digit * _fractionModifier;
+    }
+}
+
+- (void)inputDot {
+    _dotPressed = YES;
+}
+
+- (void)clear {
+    _dotPressed = NO;
+    _fractionModifier = 1;
+    self.value = 0;
+}
+
 
 @end
--- Kickstart/Shared/UI/StackCalculator/WJStackCalculatorViewController.m
+++ Kickstart/Shared/UI/StackCalculator/WJStackCalculatorViewController.m
@@ -9,17 +9,42 @@
 
 #import "WJStackCalculatorViewController.h"
 #import "WJStackCalculatorView.h"
+#import "WJStackCalculatorButtonSequenceTranslator.h"
 
 
 @interface WJStackCalculatorViewController () <WJStackCalculatorViewDelegate>
+
+@property (nonatomic, readonly) WJStackCalculatorView * calculatorView;
+
 @end
 
 @implementation WJStackCalculatorViewController {
+    WJStackCalculatorButtonSequenceTranslator *_translator;
+}
 
+- (id)init {
+    self = [super init];
+    if (self) {
+        _translator = [WJStackCalculatorButtonSequenceTranslator new];
+        [_translator addObserver:self
+                      forKeyPath:@"value"
+                         options:NSKeyValueObservingOptionNew
+                         context:nil];
+    }
+
+    return self;
+}
+
+- (void)dealloc {
+    [_translator removeObserver:self forKeyPath:@"value"];
 }
 
 - (BOOL)prefersStatusBarHidden {
     return YES;
+}
+
+- (WJStackCalculatorView *)calculatorView {
+    return (WJStackCalculatorView *) self.view;
 }
 
 - (void)loadView {
@@ -29,11 +54,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    ((WJStackCalculatorView *)self.view).delegate = self;
+    self.calculatorView.delegate = self;
+    [self synchronizeModelAndView];
 }
 
 - (void)calculatorView:(WJStackCalculatorView *)calculatorView buttonWasPressed:(WJStackCalculatorViewButtonId)buttonId {
     NSLog(@"button %d was pressed", buttonId);
+
+    if (buttonId >= WJStackCalculatorViewButtonIdNumber0 && buttonId <= WJStackCalculatorViewButtonIdNumber9) {
+        [_translator inputDigit:(buttonId - WJStackCalculatorViewButtonIdNumber0)];
+    } else if (buttonId == WJStackCalculatorViewButtonIdNumberDot) {
+        [_translator inputDot];
+    } else if (buttonId == WJStackCalculatorViewButtonIdClear) {
+        [_translator clear];
+    }
+}
+
+- (void)synchronizeModelAndView {
+    if(![self isViewLoaded]){
+        return;
+    }
+
+    [self.calculatorView.inputLabel setText:[NSString stringWithFormat:@"%f", _translator.value]];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (object == _translator && [keyPath isEqualToString:@"value"]) {
+        NSLog(@"translator changed value: %f", _translator.value);
+        [self synchronizeModelAndView];
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
 }
 
 @end
