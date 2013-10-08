bfc8b09844be5fde472c5f09f5f5176044f005b3
Antoni Kedracki <akedracki@gmail.com>
2013-10-08 22:25:34 +0200

    exercise 4B - the calculator works!!

--- Kickstart/Shared/UI/StackCalculator/WJStackCalculatorViewController.m
+++ Kickstart/Shared/UI/StackCalculator/WJStackCalculatorViewController.m
@@ -10,6 +10,7 @@
 #import "WJStackCalculatorViewController.h"
 #import "WJStackCalculatorView.h"
 #import "WJStackCalculatorButtonSequenceTranslator.h"
+#import "WJStackCalculator.h"
 
 
 @interface WJStackCalculatorViewController () <WJStackCalculatorViewDelegate>
@@ -20,6 +21,7 @@
 
 @implementation WJStackCalculatorViewController {
     WJStackCalculatorButtonSequenceTranslator *_translator;
+    WJStackCalculator * _calculator;
 }
 
 - (id)init {
@@ -30,6 +32,16 @@
                       forKeyPath:@"value"
                          options:NSKeyValueObservingOptionNew
                          context:nil];
+
+        _calculator = [WJStackCalculator new];
+        [_calculator addObserver:self
+                      forKeyPath:@"stackX"
+                         options:NSKeyValueObservingOptionNew
+                         context:nil];
+        [_calculator addObserver:self
+                      forKeyPath:@"stackY"
+                         options:NSKeyValueObservingOptionNew
+                         context:nil];
     }
 
     return self;
@@ -37,6 +49,8 @@
 
 - (void)dealloc {
     [_translator removeObserver:self forKeyPath:@"value"];
+    [_calculator removeObserver:self forKeyPath:@"stackX"];
+    [_calculator removeObserver:self forKeyPath:@"stackY"];
 }
 
 - (BOOL)prefersStatusBarHidden {
@@ -59,14 +73,29 @@
 }
 
 - (void)calculatorView:(WJStackCalculatorView *)calculatorView buttonWasPressed:(WJStackCalculatorViewButtonId)buttonId {
-    NSLog(@"button %d was pressed", buttonId);
-
     if (buttonId >= WJStackCalculatorViewButtonIdNumber0 && buttonId <= WJStackCalculatorViewButtonIdNumber9) {
         [_translator inputDigit:(buttonId - WJStackCalculatorViewButtonIdNumber0)];
     } else if (buttonId == WJStackCalculatorViewButtonIdNumberDot) {
         [_translator inputDot];
     } else if (buttonId == WJStackCalculatorViewButtonIdClear) {
         [_translator clear];
+    } else if(buttonId == WJStackCalculatorViewButtonIdPush){
+        [_calculator push:_translator.value];
+        [_translator clear];
+    } else if(buttonId == WJStackCalculatorViewButtonIdPop){
+        [_calculator pop];
+    } else if(buttonId == WJStackCalculatorViewButtonIdSwap){
+        [_calculator swap];
+    } else if(buttonId == WJStackCalculatorViewButtonIdDup){
+        [_calculator duplicate];
+    } else if(buttonId == WJStackCalculatorViewButtonIdAdd){
+        [_calculator add];
+    } else if(buttonId == WJStackCalculatorViewButtonIdSubstract){
+        [_calculator substract];
+    } else if(buttonId == WJStackCalculatorViewButtonIdMultiply){
+        [_calculator multiply];
+    } else if(buttonId == WJStackCalculatorViewButtonIdDivide){
+        [_calculator divide];
     }
 }
 
@@ -76,11 +105,13 @@
     }
 
     [self.calculatorView.inputLabel setText:[NSString stringWithFormat:@"%f", _translator.value]];
+    [self.calculatorView.stackLabel setText:[NSString stringWithFormat:@"%f\n%f", _calculator.stackX, _calculator.stackY]];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     if (object == _translator && [keyPath isEqualToString:@"value"]) {
-        NSLog(@"translator changed value: %f", _translator.value);
+        [self synchronizeModelAndView];
+    } else if (object == _calculator && ([keyPath isEqualToString:@"stackX"] || [keyPath isEqualToString:@"stackY"])) {
         [self synchronizeModelAndView];
     } else {
         [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
